6a285482e8f3a0e8948119b7c5d36ee8aeada5d8
An0 <an00nymushun@gmail.com>
2018-06-24 14:44:00 +0000

    Update SimpleDiscordCrypt.meta.js

--- SimpleDiscordCrypt.meta.js
+++ SimpleDiscordCrypt.meta.js
@@ -1,4 +1,18 @@
 // ==UserScript==
 // @name         SimpleDiscordCrypt
+// @namespace    https://gitlab.com/An0/SimpleDiscordCrypt
 // @version      0.2.3
+// @description  I hope people won't start calling this SDC ^_^
+// @author       An0
+// @license      LGPLv3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+// @downloadURL  https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/SimpleDiscordCrypt.user.js
+// @updateURL    https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/SimpleDiscordCrypt.meta.js
+// @icon         https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/logo.png
+// @match        https://discordapp.com/channels/*
+// @match        https://discordapp.com/activity
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @connect      cdn.discordapp.com
 // ==/UserScript==
