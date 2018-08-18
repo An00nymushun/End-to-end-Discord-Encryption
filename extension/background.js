befe16940827015e6eeb6ef0c61ab369fbc3a83d
An0 <an00nymushun@gmail.com>
2018-08-18 14:54:33 +0000

    Upload New File

---
+++ extension/background.js
@@ -0,0 +1,14 @@
+const onHeadersReceived = (details) => {
+	let header = details.responseHeaders.find(x => x.name.toLowerCase() === 'content-security-policy');
+	if(header != null) {
+		header.value = "";
+		return { responseHeaders: details.responseHeaders };
+	}
+};
+
+const filter = {
+  urls: ["https://discordapp.com/*","https://ptb.discordapp.com/*","https://canary.discordapp.com/"],
+  types: ["main_frame"]
+};
+
+chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, filter, ["blocking", "responseHeaders"]);
