ebf5fb7176931dbeaa341755c7767eaa7fb3cf13
An0 <an00nymushun@gmail.com>
2018-09-24 06:52:26 +0000

    Style hotfix

--- SimpleDiscordCrypt.user.js
+++ SimpleDiscordCrypt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimpleDiscordCrypt
 // @namespace    https://gitlab.com/An0/SimpleDiscordCrypt
-// @version      1.1.7
+// @version      1.1.8
 // @description  I hope people won't start calling this SDC ^_^
 // @author       An0
 // @license      LGPLv3 - https://www.gnu.org/licenses/lgpl-3.0.txt
@@ -40,7 +40,11 @@ const htmlEscapeCharacters = { "<": "&lt;", ">": "&gt;", "&": "&amp;" };
 function HtmlEscape(string) { return string.replace(/[<>&]/g, x => htmlEscapeCharacters[x]) }
 
 const Style = {
-    css: `.sdc * {
+    css: `
+/*fixes*/
+.headerBar-UHpsPw, .title-CZQTru { overflow: visible !important }
+/*style*/
+.sdc * {
 	font-family: Whitney,Helvetica Neue,Helvetica,Arial,sans-serif;
 	font-size: 16px;
 	line-height: 1;
