1c8def326df81b327110e284e444fe07883faddb
An0 <an00nymushun@gmail.com>
2018-11-11 19:19:30 +0000

    Updated the version

--- SimpleDiscordCrypt.user.js
+++ SimpleDiscordCrypt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimpleDiscordCrypt
 // @namespace    https://gitlab.com/An0/SimpleDiscordCrypt
-// @version      1.1.10
+// @version      1.1.11
 // @description  I hope people won't start calling this SDC ^_^
 // @author       An0
 // @license      LGPLv3 - https://www.gnu.org/licenses/lgpl-3.0.txt
