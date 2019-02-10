1bf326ec42236acb290bc520d02dcb9b45b0cd60
An0 <an00nymushun@gmail.com>
2019-02-10 16:00:12 +0000

    Ugly Firefox patch, updated the version for it

--- SimpleDiscordCrypt.user.js
+++ SimpleDiscordCrypt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimpleDiscordCrypt
 // @namespace    https://gitlab.com/An0/SimpleDiscordCrypt
-// @version      1.1.12
+// @version      1.1.13
 // @description  I hope people won't start calling this SDC ^_^
 // @author       An0
 // @license      LGPLv3 - https://www.gnu.org/licenses/lgpl-3.0.txt
