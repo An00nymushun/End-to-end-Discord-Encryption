4d3fba1dcc0a919bd85070a535a3bedcb152001e
An0 <an00nymushun@gmail.com>
2019-08-19 21:46:08 +0000

    Minor fixes

--- SimpleDiscordCrypt.user.js
+++ SimpleDiscordCrypt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimpleDiscordCrypt
 // @namespace    https://gitlab.com/An0/SimpleDiscordCrypt
-// @version      1.3.3
+// @version      1.3.3.1
 // @description  I hope people won't start calling this SDC ^_^
 // @author       An0
 // @license      LGPLv3 - https://www.gnu.org/licenses/lgpl-3.0.txt
@@ -1403,7 +1403,7 @@ function Init(nonInvasive)
                 response.on('data', (chunk) => data.push(chunk));
                 response.on('end', () => resolve(this.ConcatBuffers(data)));
                 response.on('aborted', reject);
-            }).on('error', reject);
+            }).on('error', reject).on('timeout', function() { this.abort() });
         })}
         : (url) => new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
@@ -2512,15 +2512,20 @@ function Init(nonInvasive)
             if(dragDelta > 10/*px*/) { parent.removeEventListener('click', closeModal); clickRemoved = true; }
         };
         const stopDrag = () => {
-            this.removeEventListener('mousemove', drag);
-            Discord.window.removeEventListener('mouseup', stopDrag);
+            let w = Discord.window;
+            w.removeEventListener('mousemove', drag);
+            w.removeEventListener('mouseup', stopDrag);
+            w.removeEventListener('blur', stopDrag);
             if(clickRemoved) setTimeout(() => parent.addEventListener('click', closeModal), 100);
         };
         this.addEventListener('mousedown', (event) => {
+            if(event.button !== 0/*Left click*/) return;
             dragDelta = 0; clickRemoved = false;
             oldX = event.clientX; oldY = event.clientY;
-            this.addEventListener('mousemove', drag);
-            Discord.window.addEventListener('mouseup', stopDrag);
+            let w = Discord.window;
+            w.addEventListener('mousemove', drag);
+            w.addEventListener('mouseup', stopDrag);
+            w.addEventListener('blur', stopDrag);
         });
         this.addEventListener('dragstart', (event) => { event.preventDefault() });
         parent.tabIndex = 0;
