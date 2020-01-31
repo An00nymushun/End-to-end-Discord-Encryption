bdbeae9ed6223821b799355022eaae565cbebad9
An0 <an00nymushun@gmail.com>
2020-01-31 15:31:07 +0000

    Replaced HtmlEscape with DOM memery

--- SimpleDiscordCrypt.user.js
+++ SimpleDiscordCrypt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimpleDiscordCrypt
 // @namespace    https://gitlab.com/An0/SimpleDiscordCrypt
-// @version      1.3.4.5
+// @version      1.3.4.6
 // @description  I hope people won't start calling this SDC ^_^
 // @author       An0
 // @license      LGPLv3 - https://www.gnu.org/licenses/lgpl-3.0.txt
@@ -53,8 +53,8 @@ const ChatInputSelector = `.scrollableContainer-2NUZem`;
 const MessageImgSelector = `.message-1PNnaP img`;
 const ChatImageSelector = `${MessageContainerSelector} .imageZoom-1n-ADA img`;
 
-const htmlEscapeCharacters = { "<": "&lt;", ">": "&gt;", "&": "&amp;" };
-function HtmlEscape(string) { return string.replace(/[<>&]/g, x => htmlEscapeCharacters[x]) }
+const htmlEscapeDiv = document.createElement('div');
+function HtmlEscape(string) { htmlEscapeDiv.textContent = string; return htmlEscapeDiv.innerHTML; }
 
 const Style = {
     css: `
